419d5fc0c42801991ff6ad157c79c2d7b271c5fb
Hasan-Mac <hasan@klapify.com>
2022-08-22 11:35:35 +0600

    Initial Commit

---
+++ ItunesSearchAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ItunesSearchAPI
+//
+//  Created by Hasan on 22/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ItunesSearchAPI/ItunesSearchAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesSearchAPIApp.swift
+//  ItunesSearchAPI
+//
+//  Created by Hasan on 22/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ItunesSearchAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
